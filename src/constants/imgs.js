e56df0c68d5ccc34287f86ed6be55362f10756eb
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-04-19 19:39:27 +0100

    feat: add logo page

--- src/constants/imgs.js
+++ src/constants/imgs.js
@@ -1,5 +1,6 @@
 export default {
   bgPattern: require('../assets/drawer-cover.jpeg'),
-  user: require('../assets/user.jpg'),
+  //user: require('../assets/user.jpg'),
+  user: require('../assets/lindo.jpeg'),
   logo: require('../assets/logo.png')
 };
