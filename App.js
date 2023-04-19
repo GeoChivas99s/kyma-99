a995a896b3e948d73e16c45885f44f9c62c63cc0
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-04-19 11:54:50 +0100

    feat: first configs

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+import {DrawerActions, NavigationContainer} from '@react-navigation/native';
+
+import AuthNavigator from './src/navigations/AuthNavigator';
 
 export default function App() {
+  // isAuthenticated = is...
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* {isAuthenticated ? AuthNavigator : DrawerNavigator } */}
+      <AuthNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
