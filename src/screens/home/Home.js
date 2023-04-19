3e10b2add4dd5feafe5812dede52e3fe6fea70c6
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-04-20 00:22:30 +0100

    feat: add logo page

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,22 +1,69 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
-import { COLORS, IMGS } from '../../constants';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { COLORS, IMGS, ROUTES } from '../../constants';
+import Icon from 'react-native-vector-icons/Ionicons';
+import { useNavigation } from "@react-navigation/native";
+
+const menuOptions = [
+  {
+    name: "leitura",
+    icon: "book",
+    color: "red",
+    route: ROUTES.HOME
+  }
+
+];
+
+
 
 const Home = () => {
+  const navigation = useNavigation();
+  // const MenuItem = ({ route, color, name, icon }) => {
+  //   return (
+  //     <View style={{borderWidth:4}}>
+  //       <TouchableOpacity onPress={() => navigation.navigate(route)}>
+  //         <Icon name={icon} size={50} color={COLORS.black} />
+  //         <Text>{name}</Text>
+  //       </TouchableOpacity>
+  //     </View>
+
+  //   )
+  // }
+  const MenuItem = () => {
+    return (
+      <View >
+        <TouchableOpacity style={styles.buttonWithIcon}>
+          <Icon name={"book"} size={30} color={COLORS.white} />
+        </TouchableOpacity>
+        <Text>Sambapito</Text>
+      </View>
+
+    )
+  }
   return (
     <View style={styles.container}>
       <View style={styles.bannerWrapper}>
         <Image style={styles.img} source={IMGS.banner} />
         <View style={styles.overlay}>
         </View>
-          <View style={styles.bannerContent}>
-            <Text style={styles.bannerContentText}>Bem vindo ao seu assistente de terapia!</Text>
-            <Text style={styles.bannerContentText}>Kyma</Text>
-          </View>
+        <View style={styles.bannerContent}>
+          <Text style={styles.bannerContentText}>Bem vindo ao seu assistente de terapia!</Text>
+          <Text style={styles.bannerContentText}>Kyma</Text>
+        </View>
       </View>
-      <View style={styles.menuArea}>
-        <Text> Bom dia </Text>
-      </View>
+      <ScrollView style={styles.menuArea} alignItems="center">
+    
+
+
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+        <MenuItem />
+
+      </ScrollView>
     </View>
   );
 };
@@ -30,8 +77,7 @@ const styles = StyleSheet.create({
     flex: 1
   },
   bannerWrapper: {
-    flex: 1,
-    borderWidth: 1,
+    flex: 1/2,
     width: "100%",
 
   }, img: {
@@ -48,19 +94,26 @@ const styles = StyleSheet.create({
 
   }, bannerContent: {
     position: "absolute",
-    alignSelf:"center",
-    top:40,
-    zIndex:5,
-    justifyContent:"center",
-    alignItems:"center",
+    alignSelf: "center",
+    top: 40,
+    zIndex: 5,
+    justifyContent: "center",
+    alignItems: "center",
     width: "100%",
     height: "100%",
-  },bannerContentText:{
-    color:COLORS.white,
-    fontSize:18
+  }, bannerContentText: {
+    color: COLORS.white,
+    fontSize: 18
   },
   menuArea: {
-    flex: 2
+    flex: 3,
+    padding:45,
+    borderWidth: 3,
+    alignContent: "center",
+    // alignItems:"center"
+  },
+  buttonWithIcon: {
+    borderRadius: 50, padding: 20, justifyContent: "center", alignItems: "center", backgroundColor: COLORS.warning
   }
 
 });
