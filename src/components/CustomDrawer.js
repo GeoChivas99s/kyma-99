29e761f2511a69f7e489c228e83d03487de399cf
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-04-20 14:04:09 +0100

    feat: add logo page

--- src/components/CustomDrawer.js
+++ src/components/CustomDrawer.js
@@ -3,25 +3,35 @@ import {
   ImageBackground,
   StyleSheet,
   Image,
+  Text,
   View,
   Dimensions,
+  TouchableOpacity
 } from 'react-native';
 import {
   DrawerContentScrollView,
   DrawerItemList,
 } from '@react-navigation/drawer';
-import {COLORS, IMGS} from '../constants';
-
-const {width} = Dimensions.get('screen');
+import { COLORS, IMGS } from '../constants';
+import Icon from 'react-native-vector-icons/Ionicons';
+const { width } = Dimensions.get('screen');
 
 const CustomDrawer = props => {
   return (
     <DrawerContentScrollView {...props}>
-      <ImageBackground source={IMGS.banner} style={{height: 140}}>
+      <ImageBackground source={IMGS.banner} style={{ height: 140 }}>
         <Image source={IMGS.user} style={styles.userImg} />
       </ImageBackground>
       <View style={styles.drawerListWrapper}>
         <DrawerItemList {...props} />
+      </View>
+      <View style={{position:"absolute",top:700, width:"100%"}} >
+        <TouchableOpacity  style={styles.exitButton}>
+          <Text style={{color:COLORS.white, marginRight:12, fontSize:16}}>
+            Sair
+          </Text>
+          <Icon name='exit' color={COLORS.white} size={20}/>
+        </TouchableOpacity>
       </View>
     </DrawerContentScrollView>
   );
@@ -42,5 +52,15 @@ const styles = StyleSheet.create({
   },
   drawerListWrapper: {
     marginTop: 65,
-  },
+  },exitButton:{
+   flexDirection:"row",
+   width:"50%",
+   alignItems:"center",
+   justifyContent:"center",
+   padding:10,
+   marginLeft:15,
+   borderRadius:5,
+   backgroundColor:COLORS.danger
+    
+  }
 });
