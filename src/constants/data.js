8a1e07db87421b07ce895bb75f1360588b90503d
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-04-25 12:41:56 +0100

    feat: add logo page

---
+++ src/constants/data.js
@@ -0,0 +1,77 @@
+import { IMGS } from "."
+
+
+export default [
+
+    {
+        img: IMGS.step1,
+        title: "Respire fundo e com controle quando for se preparar para falar",
+        description: `Os sintomas da gagueira podem piorar quando se está ansioso. Portanto, faça exercícios de respiração profunda para relaxar o corpo antes de interagir com outras pessoas para conseguir falar melhor`,
+        steps:`Faça exercícios de respiração regularmente para reduzir a ansiedade.
+        Esses exercícios são ainda mais eficazes antes de interações sociais, que causam sintomas de ansiedade e gagueira.`,
+        id: 1
+    },
+    {
+        img: IMGS.step2,
+        title: "Fale olhando para o espelho",
+        description: `Às vezes, você pode aprender a analisar os seus padrões de fala olhando o seu reflexo no espelho. Preste atenção às palavras, aos sons e às frases que causam gagueira.[3]`,
+        steps:`Faça contato visual consigo mesmo o tempo todo. Isso é importante porque simula uma conversa com outra pessoa, na qual esse contato é indispensável.
+        Você pode ainda visualizar outra pessoa no espelho e imaginar que está conversando com ela para se preparar.
+        Comece o exercício sozinho e, aos poucos, inclua parentes e amigos. As pessoas podem estranhar um pouco, mas explique que é porque você gagueja menos quando está (ou acha que está) sozinho. A presença de outros ajuda nessa parte da análise.`,
+        id: 2
+    },
+    {
+        img: IMGS.step3,
+        title: "Grave a si mesmo falando",
+        description: `É mais fácil analisar os seus padrões de fala com este método do que com o método do espelho. Monte a câmera e fale olhando para ela — mais uma vez, começando sozinho e, depois, com a ajuda de amigos ou parentes. Quando terminar, assista ao vídeo e veja o que acha`,
+        steps:`Analise a gravação com a ajuda de amigos e parentes. Pode ser que eles notem detalhes dos seus padrões de fala que nem você nota.`,
+        id: 3
+    },
+    {
+        img: IMGS.step4,
+        title: "Faça uma lista das palavras que causam o problema",
+        description: `No geral, pessoas que gaguejam têm problemas com palavras, frases e sons específicos. Fique atento à sua fala e tente identificar alguns desses termos.`,
+        steps:`Antes de ensaiar com os métodos deste artigo, você pode evitar usar esses termos em público e ver o que acontece. Como tempo, vai ser mais fácil usá-los no dia a dia sem começar a gaguejar.`,
+        id: 4
+    },
+    {
+        img: IMGS.step5,
+        title: "Ensaie com as palavras que causam a gagueira",
+        description: `Depois de identificar os termos que causam a gagueira, concentre-se neles durante os ensaios. Repita as frases, os sons e as palavras para se dessensibilizar.`,
+        steps:`Concentre-se primeiro em repetir esses termos bem devagar, sempre respirando fundo com calma. Como ainda é o início, não se preocupe se você gaguejar.
+        Quando você conseguir dizer melhor esses termos individualmente, comece a formar frases completas, ainda falando devagar e com calma.
+        `,
+        id: 5
+    },
+    {
+        img: IMGS.step6,
+        title: "Prolongue a primeira sílaba de cada palavra",
+        description: `Essa prática, conhecida como prolongamento, aumenta o foco e reduz a tensão que leva a pessoa à gagueira. Fale com o máximo de calma que você conseguir, atentando-se a cada sílaba individual.`,
+        steps:`Concentre-se ainda mais nos termos que causam a gagueira. Divida esses sons e palavras em sessões para conseguir cumprir o exercício.
+        Não se preocupe se você gaguejar durante os exercícios. O objetivo não é falar sem gaguejar, mas sim manter a calma durante a fala.`,
+        id: 6
+    },
+    {
+        img: IMGS.step7,
+        title: "Faça exercícios de ritmo",
+        description: `As pessoas gaguejam menos quando estão cantando, já que falar com um ritmo previsível impede o cérebro de se confundir e tropeçar na pronúncia.`,
+        steps:`Por exemplo: ensaie com músicas que você conhece e gosta de ouvir para se divertir enquanto aprende.`,
+        id: 7
+    },
+    {
+        img: IMGS.step8,
+        title: "Leia em voz alta",
+        description: ` Esse exercício também ajuda a acostumar a pessoa à pronúncia das palavras. Preste atenção a cada sílaba individual, começando com uma passagem que você já conhece. Depois, passe a ler trechos que nunca leu antes para treinar o cérebro a situações mais complexas`,
+        steps:`Mais uma vez, não se preocupe se você gaguejar enquanto lê. Continue o exercício.
+        Misture atividades de leitura e ritmo. Por exemplo: leia um texto enquanto bate as mãos na mesa em intervalos regulares.
+        Use a técnica do prolongamento para ler, concentrando-se em falar bem devagar e com calma.`,
+        id: 8
+    },
+    {
+        img: IMGS.step9,
+        title: "Converse com as pessoas ao telefone",
+        description: `Falar ao telefone é uma estratégia excelente para quem quer ensaiar, mas não está pronto para interações presenciais. Portanto, ligue para os seus amigos e parentes e use as técnicas listadas acima durante a conversa`,
+        steps:`Você pode ainda ligar para os serviços de atendimento ao consumidor de algumas lojas em vez de enviar e-mails e mensagens por celular.`,
+        id: 9
+    }
+]
--- src/constants/imgs.js
+++ src/constants/imgs.js
@@ -4,5 +4,14 @@ export default {
   user: require('../assets/lindo.jpeg'),
   logo: require('../assets/logo.png'),
   step1: require('../assets/Step-1.jpg.jpeg'),
-  
+  step2: require('../assets/Step-2.jpg.jpeg'),
+  step3: require('../assets/Step-3.jpg.jpeg'),
+  step4: require('../assets/Step-4.jpg.jpeg'),
+  step5: require('../assets/Step-5.jpg.jpeg'),
+  step6: require('../assets/Step-6.jpg.jpeg'),
+  step7: require('../assets/Step-7.jpg.jpeg'),
+  step8: require('../assets/Step-8.jpg.jpeg'),
+  step9: require('../assets/Step-9.jpg.jpeg'),
+  step10V2: require('../assets/Step-10-V2.jpg.jpeg'),
+
 };
