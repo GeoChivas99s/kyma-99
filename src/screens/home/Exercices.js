c3dad4b8d4df8e2c817b3495e78a21ad2f3d76eb
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-04-25 15:42:49 +0100

    feat: add logo page

--- src/screens/home/Exercices.js
+++ src/screens/home/Exercices.js
@@ -1,17 +1,34 @@
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import React from 'react';
-import {COLORS} from '../../constants';
-
+import { COLORS } from '../../constants';
+import Svg, { Path } from 'react-native-svg';
 const Wallet = () => {
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      //  backgroundColor: COLORS.grayLight,
+        flex: 1
       }}>
-      <Text>Wallet!</Text>
+    
+        <View style={{ height: 120, backgroundColor: COLORS.primary, flex:1/6  }}>
+          <Svg
+            height={300}
+            width={Dimensions.get("screen").width}
+            viewBox='0 0 1440 320'
+          >
+            <Path
+              fill={COLORS.primary}
+              d='M0,192L48,176C96,160,192,128,288,122.7C384,117,480,139,576,165.3C672,192,768,224,864,229.3C960,235,1056,213,1152,181.3C1248,149,1344,107,1392,85.3L1440,64L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z'
+            />
+          </Svg>
+
+        </View>
+    
+      <View style={{flex:1, marginTop:-40, paddingLeft:10}}>
+        <Text style={{textTransform:"uppercase", fontSize:18 ,color:COLORS.white}}>Terapias Individuais</Text>
+        <View>
+
+        </View>
+      </View>
     </View>
   );
 };
