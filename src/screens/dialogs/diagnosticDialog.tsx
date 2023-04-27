c1ec06754cd7119ee520d2991db0970b6c9ce2a9
Geovane Chivas <geovane.chivaluca@drcash.com.br>
2023-04-27 12:10:21 +0100

    feat: add logo page

--- src/screens/dialogs/diagnosticDialog.tsx
+++ src/screens/dialogs/diagnosticDialog.tsx
@@ -1,25 +1,115 @@
-
-import React, { Children, useState } from 'react';
-import Icon from 'react-native-vector-icons/Ionicons';
-import { COLORS, IMGS, ROUTES } from '../../constants';
+import React, { Children, useState } from "react";
+import Icon from "react-native-vector-icons/Ionicons";
+import { COLORS, IMGS, ROUTES } from "../../constants";
 import { useNavigation } from "@react-navigation/native";
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
-import KymaModal from '../../components/KymaModal';
-
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+import KymaModal from "../../components/KymaModal";
 
 const DiagnosticDialog = () => {
+  return (
+    <KymaModal title="Diagnóstico">
+      <Text></Text>
+      <View
+        style={{
+          width: "100%",
+          height: "90%",
+          marginTop: 50,
+          flexWrap: "wrap",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <View
+          style={{
+            padding: 20,
+            backgroundColor: "#1F6C65",
+            marginLeft: 10,
+            borderRadius:10,
+            overflow:"hidden",
+            width:"95%",
+            flexDirection:"row",
+            alignItems:"center",
+            justifyContent:"space-between"
+          }}
+        >
+           <Text style={{fontSize: 25, color: "white",}}>Severidade:</Text> 
+           <Text style={{fontSize: 25, color: "white",}}>Moderada</Text>
+        </View>
 
-    return (
-        <KymaModal title="Diagnóstico" >
-            <View style={{ width:"100%", height:"90%",marginTop:30}}>
-
-                <Text>Lindo mesmo!!!</Text>
-
-            </View>
-        </KymaModal>
-    )
-
-
-}
+        <View
+          style={{
+            width: "100%",
+            height: "90%",
+            padding: 10,
+            flexWrap: "wrap",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <DialogCard />
+          <DialogCard />
+          <DialogCard />
+          <DialogCard />
+        </View>
+      </View>
+    </KymaModal>
+  );
+};
 
 export default DiagnosticDialog;
+
+const DialogCard = () => {
+  return (
+    <View
+      style={{
+        borderWidth: 1,
+        height: 110,
+        width: 150,
+        borderRadius: 10,
+        padding: 10,
+        paddingBottom: 15,
+        backgroundColor: "#1F6C65",
+        justifyContent: "space-between",
+        marginBottom: 15,
+      }}
+    >
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+        }}
+      >
+        <View
+          style={{
+            borderWidth: 1,
+            width: 50,
+            backgroundColor: "white",
+            height: 50,
+            justifyContent: "center",
+            padding: 3,
+            alignItems: "center",
+            borderRadius: 10,
+          }}
+        >
+          <Icon name="mic" size={30} color="#1F6C65" />
+        </View>
+
+        <Text style={{ color: "white", fontSize: 28 }}>16x</Text>
+      </View>
+
+      <Text
+        style={{ color: "white", textTransform: "uppercase", fontSize: 16 }}
+      >
+        Prolongações
+      </Text>
+    </View>
+  );
+};
--- src/screens/home/Diagnostic.tsx
+++ src/screens/home/Diagnostic.tsx
@@ -144,7 +144,7 @@ const Diagnostic = () => {
         });
       }
     });
-  //  setOpenModal(true);
+   setOpenModal(true);
 
   }, []);
 
